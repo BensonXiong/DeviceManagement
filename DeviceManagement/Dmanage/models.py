577337af9ce20b36d826d7992cac56e384abc1ed
BensonXiong <benson_xiong@sina.com>
2015-12-22 23:06:16 +0800

    b

--- DeviceManagement/Dmanage/models.py
+++ DeviceManagement/Dmanage/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from pip._vendor.cachecontrol.heuristics import LastModified
 
 class Device(models.Model):
-    type = models.CharField()
-    name = models.CharField()
-    version = models.CharField()
-    model = models.CharField()
-    imei = models.CharField()
-    sn = models.CharField()
-    originOwner = models.CharField()
-    borrower = models.CharField()
+    type = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    version = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    imei = models.CharField(max_length=128,unique=True)
+    sn = models.CharField(max_length=128,unique=True)
+    originOwner = models.CharField(max_length=128)
+    borrower = models.CharField(max_length=128)
     borrowedAt = models.DateTimeField()
     returnAt = models.DateTimeField()
-    createdAt = models.DateTimeField()
+    createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField()
+    LastModifiedAt = models.DateTimeField(auto_now=True)
     
